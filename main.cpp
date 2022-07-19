623f01f765dffb199f42bf6d4ad8d627ee9c3931
张林博(Linbo.Z) <linbo.zhang@mihoyo.com>
2022-07-19 15:44:10 +0800

    create logical device

--- main.cpp
+++ main.cpp
@@ -182,7 +182,26 @@ private:
 	}
 	void createLogicalDevice()
 	{
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount = 1;
+		float queuePriority = 1.0f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
 		
+		VkPhysicalDeviceFeatures deviceFeatures{};
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+		createInfo.pEnabledFeatures = &deviceFeatures;
+
+		createInfo.enabledExtensionCount = 0;
+		if(vkCreateDevice(physicalDevice,&createInfo,nullptr,&device)!=VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create logicaldevice");
+		}
 	}
 	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
 	{
@@ -299,6 +318,7 @@ private:
 	}
 	void cleanup()
 	{
+		vkDestroyDevice(device, nullptr);
 		if(enableValidationLayers)
 		{
 			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
