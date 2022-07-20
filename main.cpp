a3a84f30d5dd7b984aeba0b67ede9e47a59bbea9
张林博(Linbo.Z) <linbo.zhang@mihoyo.com>
2022-07-20 14:55:09 +0800

    choosing the right settings forthe swap chain

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <optional>
 #include <set>
+#include <algorithm>
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
@@ -324,6 +325,43 @@ private:
 		if (physicalDevice == VK_NULL_HANDLE)
 		{
 			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+	}
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
+	{
+		for(const auto& availableFormat:availableFormats)
+		{
+			if(availableFormat.format==VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace==VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+			{
+				return availableFormat;
+			}
+		}
+		return availableFormats[0];
+	}
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes)
+	{
+		for(const auto& availablePresentMode:availablePresentModes)
+		{
+			if(availablePresentMode==VK_PRESENT_MODE_MAILBOX_KHR)
+			{
+				return availablePresentMode;
+			}
+		}
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities)
+	{
+		if(capabilities.currentExtent.width!=UINT32_MAX)
+		{
+			return capabilities.currentExtent;
+		}else
+		{
+			VkExtent2D actualExtent = { WIDTH,HEIGHT };
+			actualExtent.width = std::max(capabilities.minImageExtent.width, std::min(capabilities.maxImageExtent.width, actualExtent.width));
+			actualExtent.height = std::max(capabilities.minImageExtent.height, std::min(capabilities.maxImageExtent.height, actualExtent.height));
+			return actualExtent;
+			
 		}
 	}
 
