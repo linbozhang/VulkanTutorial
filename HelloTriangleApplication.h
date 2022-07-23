ee05a64684be516b63d4dec5bda585cf16c9472e
张林博(Linbo.Z) <linbo.zhang@mihoyo.com>
2022-07-23 15:30:03 +0800

    sperate sorce code to other files

---
+++ HelloTriangleApplication.h
@@ -0,0 +1,99 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <optional>
+#include <set>
+#include <algorithm>
+#include <fstream>
+
+struct QueueFamilyIndices
+{
+	std::optional<uint32_t> graphicsFamily;
+	std::optional<uint32_t> presentFamily;
+	bool isComplete()
+	{
+		return graphicsFamily.has_value() && presentFamily.has_value();
+	}
+};
+struct SwapChainSupportDetails
+{
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+};
+class HelloTriangleAppliaction
+{
+public:
+	void run();
+private:
+	GLFWwindow* window = nullptr;
+	VkInstance instance = nullptr;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice logicalDevice = nullptr;
+	VkQueue graphicsQueue = nullptr;
+	VkQueue presentQueue = nullptr;
+	VkSurfaceKHR surface;
+	VkDebugUtilsMessengerEXT debugMessenger = nullptr;
+	VkSwapchainKHR swapChain;
+	std::vector<VkImage> swapChainImages;
+	std::vector<VkImageView> swapChainImageViews;
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+	VkRenderPass renderPass;
+	VkPipelineLayout pipelineLayout;
+	VkPipeline graphicsPipeline;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+	VkCommandPool commandPool;
+	std::vector<VkCommandBuffer> commandBuffers;
+	VkSemaphore imageAvailableSemaphore;
+	VkSemaphore renderFinishedSemaphore;
+
+	void initWindow();
+	void initVulkan();
+	void mainLoop();
+	void drawFrame();
+	void cleanup();
+	void createCommandBuffers();
+	void createCommandPool();
+	void createFrameBuffers();
+	void createInstance();
+	bool checkValidationLayerSupport();
+	VkShaderModule createShaderModule(const std::vector<char>& code);
+	void createRenderPass();
+	void createGraphicsPipeline();
+	std::vector<const char*> getRequiredExtensions();
+	void createSwapChain();
+	void createImageViews();
+	void createSurface();
+	void createLogicalDevice();
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+	QueueFamilyIndices findQueueFamilies(const VkPhysicalDevice device);
+	void pickPhysicalDevice();
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+	int rateDeviceSuitability(VkPhysicalDevice device);
+	void setupDebugMessenger();
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData)
+	{
+		if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+		{
+			std::cerr << "validation layer:" << pCallbackData->pMessage << std::endl;
+		}
+
+		return VK_FALSE;
+	}
+};
